2f40b25046880b675e66e2e311c1098c103a6587
Piriya <piriya.muaithaisong@gmail.com>
2022-11-11 21:41:22 +0700

    Set up catagories

--- capstone-react/src/App.js
+++ capstone-react/src/App.js
@@ -1,41 +1,38 @@
 const App = () => {
+  const catagories = [
+    {
+      id: 1,
+      titile: "Hats",
+    },
+    {
+      id: 2,
+      titile: "Jackets",
+    },
+    {
+      id: 3,
+      titile: "Sneakers",
+    },
+    {
+      id: 4,
+      titile: "Womens",
+    },
+    {
+      id: 5,
+      titile: "Mens",
+    },
+  ];
+
   return (
     <div className="catagories-container">
-      <div className="catagoriy-container">
-        {/* <img /> */}
-        <div className="categoty-body-container">
-          <h2>Hats</h2>
-          <p>Shop now</p>
+      {catagories.map((catagory) => (
+        <div className="catagory-container">
+          {/* <img /> */}
+          <div id={catagory.id} className="categoty-body-container">
+            <h2>{catagory.titile}</h2>
+            <p>Shop now</p>
+          </div>
         </div>
-      </div>
-      <div className="catagoriy-container">
-        {/* <img /> */}
-        <div className="categoty-body-container">
-          <h2>Jackets</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="catagoriy-container">
-        {/* <img /> */}
-        <div className="categoty-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="catagoriy-container">
-        {/* <img /> */}
-        <div className="categoty-body-container">
-          <h2>Womens</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="catagoriy-container">
-        {/* <img /> */}
-        <div className="categoty-body-container">
-          <h2>Mens   </h2>
-          <p>Shop now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
