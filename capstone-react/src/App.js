97b27af8accbef33de98c48b60a1b8aa3189d971
Piriya <piriya.muaithaisong@gmail.com>
2022-11-14 22:31:22 +0700

    Add placeholder for the navigation bar

--- capstone-react/src/App.js
+++ capstone-react/src/App.js
@@ -1,11 +1,23 @@
 import Home from "./routes/home/home.component";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Outlet } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the Navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
-      
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
