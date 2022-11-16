a1fe1b892b078b786331a80a0e911852f44fd850
Piriya <piriya.muaithaisong@gmail.com>
2022-11-16 22:36:12 +0700

    Add sign-in form

--- capstone-react/src/routes/sign-in/sign-in.component.jsx
+++ capstone-react/src/routes/sign-in/sign-in.component.jsx
@@ -1,39 +1,39 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 
+import SignUpFrom from "../../components/sing-up-from/sign-up-from.component";
 import {
   auth,
   signinWithGooglePopup,
   createUserDocumetFromAuth,
-  signInWithGoogleRedirect,
+  //signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
-const getDataFromRedirected = async () => {
-  const response = await getRedirectResult(auth);
-  if (response){
-    const userDocRef = createUserDocumetFromAuth(response.user);
-  }
-};
+// const getDataFromRedirected = async () => {
+//   const response = await getRedirectResult(auth);
+//   if (response){
+//     const userDocRef = createUserDocumetFromAuth(response.user);
+//   }
+// };
 
 const SignIn = () => {
-  useEffect(() => {
-    getDataFromRedirected();
-  }, []);
+  // useEffect(() => {
+  //   getDataFromRedirected();
+  // }, []);
 
   const logGoogleUser = async () => {
     const { user } = await signinWithGooglePopup();
     const userDocRef = createUserDocumetFromAuth(user);
-
-    //console.log(user);
   };
 
   return (
     <div>
       <h1>Sign In page</h1>
       <button onClick={logGoogleUser}>Sign in With Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
+      {/* <button onClick={signInWithGoogleRedirect}>
         Sign in With Google Redirect
-      </button>
+      </button> */}
+      <SignUpFrom />
     </div>
   );
 };
