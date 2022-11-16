26c57ad41311bc4d3f9de2c0fc7725f25cd80b52
Piriya <piriya.muaithaisong@gmail.com>
2022-11-16 21:52:41 +0700

    Add sign-in with redirect

--- capstone-react/src/routes/sign-in/sign-in.component.jsx
+++ capstone-react/src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,29 @@
-import { signinWithGooglePopup, createUserDocumetFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import {
+  auth,
+  signinWithGooglePopup,
+  createUserDocumetFromAuth,
+  signInWithGoogleRedirect,
+} from "../../utils/firebase/firebase.utils";
+
+const getDataFromRedirected = async () => {
+  const response = await getRedirectResult(auth);
+  if (response){
+    const userDocRef = createUserDocumetFromAuth(response.user);
+  }
+};
 
 const SignIn = () => {
+  useEffect(() => {
+    getDataFromRedirected();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signinWithGooglePopup();
     const userDocRef = createUserDocumetFromAuth(user);
-    
+
     //console.log(user);
   };
 
@@ -12,6 +31,9 @@ const SignIn = () => {
     <div>
       <h1>Sign In page</h1>
       <button onClick={logGoogleUser}>Sign in With Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in With Google Redirect
+      </button>
     </div>
   );
 };
