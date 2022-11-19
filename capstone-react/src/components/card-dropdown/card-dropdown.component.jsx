6d490ba402b86cda2327ddf90a4c7b113791c1d3
Piriya <piriya.muaithaisong@gmail.com>
2022-11-19 23:43:45 +0700

    Create Cart

---
+++ capstone-react/src/components/card-dropdown/card-dropdown.component.jsx
@@ -0,0 +1,16 @@
+import Button from '../button/button.component';
+import './card-dropdown.styles.scss'
+
+
+const CartDropdown = () => {
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='class-item'>
+
+            </div>
+            <Button>CHECKOUT</Button>
+        </div>
+    )
+}
+
+export default CartDropdown;
--- capstone-react/src/routes/navigation/navigation.component.jsx
+++ capstone-react/src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,8 @@ import { UserContext } from "../../context/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/card-dropdown/card-dropdown.component";
 // Link is  the a tag but better
 
 const Navigation = () => {
@@ -32,7 +34,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
