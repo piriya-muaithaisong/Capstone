7825b3e91bfc00818481323f5f3aea824209ef2a
Piriya <piriya.muaithaisong@gmail.com>
2022-11-19 23:05:06 +0700

    Complete Product card component

--- capstone-react/src/routes/shop/shop.component.jsx
+++ capstone-react/src/routes/shop/shop.component.jsx
@@ -1,12 +1,14 @@
-import SHOP_DATA from "../../shop-data.json";
+import { ProductsContext } from "../../context/product.context";
+import { useContext } from "react";
+import ProductCard from "../../components/product-card/product-card.component";
+import './shop.styles.scss'
 
 const Shop = () => {
+  const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {SHOP_DATA.map(({ id, name }) => (
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product}></ProductCard>
       ))}
     </div>
   );
