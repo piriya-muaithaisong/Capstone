dcb4cbbe2b6689f2e331b2ea8c660dfa6d38bf72
Piriya <piriya.muaithaisong@gmail.com>
2022-11-27 20:45:06 +0700

    styled Directory item and cart icon

--- capstone-react/src/components/directory-item/directory-item.component.jsx
+++ capstone-react/src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,19 @@
-import "./directory-item.styles.scss";
+import { BackgroundImage, Body, DirectoryItemContainer } from "./directory-item.styles";
+
 
 const DirectoryItem = ({ category }) => {
   const { title, imageUrl } = category;
 
   return (
-    <div className="directory-item-container">
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
+    <DirectoryItemContainer>
+      <BackgroundImage
+        imageUrl={imageUrl}
       />
-      <div className="body">
+      <Body>
         <h2>{title}</h2>
         <p>Shop now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 
