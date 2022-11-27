ef6c669838abf023d670a889b00e8b9a636be382
Piriya <piriya.muaithaisong@gmail.com>
2022-11-27 19:07:29 +0700

    styled Button

--- capstone-react/src/components/sing-in-from/sign-in-from.component.jsx
+++ capstone-react/src/components/sing-in-from/sign-in-from.component.jsx
@@ -4,7 +4,7 @@ import {
   createUserDocumetFromAuth,
   signInAuthUserWithEmailAndPassword,
 } from "../../utils/firebase/firebase.utils";
-import Button from "../button/button.component";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 import FromInput from "../form-input/form-input.componet";
 import "./sign-in-form.styles.scss";
 
@@ -77,7 +77,7 @@ const SignInFrom = () => {
 
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+          <Button type="button" buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>
             Google Sign In
           </Button>
         </div>
