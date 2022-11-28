706b634c5f89edf76588dda54522fe9374ee2364
Piriya <piriya.muaithaisong@gmail.com>
2022-11-28 23:18:18 +0700

    Chnge user context to reducer style

--- capstone-react-w-redux/src/context/user.context.jsx
+++ capstone-react-w-redux/src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumetFromAuth,
@@ -11,9 +11,39 @@ export const UserContext = createContext({
   setCurrentuser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandle type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 // provider allow childer to access the value on value tag
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentuser] = useState(null);
+  //const [currentUser, setCurrentuser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  console.log(state)
+
+  const { currentUser } = state;
+  const setCurrentuser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentuser };
 
   //signOutUser();
@@ -31,3 +61,5 @@ export const UserProvider = ({ children }) => {
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+
