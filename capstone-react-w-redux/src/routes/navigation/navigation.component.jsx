b1f49e46e924f92232315c2eaefc5129485fbc0e
Piriya <piriya.muaithaisong@gmail.com>
2022-12-04 22:56:18 +0700

    Add user selector

--- capstone-react-w-redux/src/routes/navigation/navigation.component.jsx
+++ capstone-react-w-redux/src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,22 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment, useContext } from "react";
-import { UserContext } from "../../context/user.context";
+
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { NavigationContainer, LogoContainer, NavLink, NavLinks } from "./navigation.styles";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/card-dropdown/card-dropdown.component";
 import { CartContext } from "../../context/cart.context";
+
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../store/user/user.selector";
+
 // Link is  the a tag but better
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  //const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser)
+
   const { isCartOpen } = useContext(CartContext);
 
   const signOutHandler = async () => {
