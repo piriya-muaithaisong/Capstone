88954c421bc4bb827eb0f5c14392cd92adacc1a9
Piriya <piriya.muaithaisong@gmail.com>
2022-12-08 18:46:17 +0700

    Manually create logger middleware

--- capstone-react-w-redux/src/store/store.js
+++ capstone-react-w-redux/src/store/store.js
@@ -1,10 +1,34 @@
 import { createStore, compose, applyMiddleware } from "redux";
-import logger from "redux-logger";
+//import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
+/*
+const curryFuc = (a) => (b,c) => {
+    a + b - c
+}
+
+const withA = curryFuc(3);
+
+withA(2,3) // 3+2-4 = 1
+*/
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type){
+        return next()
+    }
+
+    console.log('type: ', action.type)
+    console.log('payload: ', action.payload)
+    console.log('currentState: ', store.getState())
+
+    next(action)
+
+    console.log('NewState: ', store.getState())
+} 
+
 // root-reducer
-const middleWares = [logger];
+const middleWares = [loggerMiddleware];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(rootReducer, undefined, composedEnhancers);
