c458e5d32303a7288b5b66bb6bf79d58a0746e41
Piriya <piriya.muaithaisong@gmail.com>
2022-12-09 23:58:33 +0700

    Migrate checkout component to redux

--- capstone-react-w-redux/src/components/checkout-item/checkout-item.component.jsx
+++ capstone-react-w-redux/src/components/checkout-item/checkout-item.component.jsx
@@ -1,14 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectCartItems } from "../../store/cart/cart.selector";
+import {
+  clearItemFromCart,
+  addItemToCart,
+  removeItemFromCart,
+} from "../../store/cart/cart.action";
 import "./checkout-item.styles.scss";
-import { CartContext } from "../../context/cart.context";
-import { useContext } from "react";
 
 const CheckOutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
-  const { addItemToCart, removeItemFromCart, clearItemFromCart } =
-    useContext(CartContext);
-  const clearItemHandler = () => clearItemFromCart(cartItem);
-  const addItemHandler = () => addItemToCart(cartItem);
-  const removeItemHandler = () => removeItemFromCart(cartItem);
+  const dispatch = useDispatch();
+  const cartItems = useSelector(selectCartItems);
+
+  const clearItemHandler = () =>
+    dispatch(clearItemFromCart(cartItems, cartItem));
+  const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+  const removeItemHandler = () =>
+    dispatch(removeItemFromCart(cartItems, cartItem));
 
   return (
     <div className="checkout-item-container">
