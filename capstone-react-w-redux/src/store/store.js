277ca0998fcd74b0bf04d2b7c98a3cfbd19ba0aa
Piriya <piriya.muaithaisong@gmail.com>
2022-12-10 22:54:45 +0700

    Add persistance use redux persist

--- capstone-react-w-redux/src/store/store.js
+++ capstone-react-w-redux/src/store/store.js
@@ -1,4 +1,6 @@
 import { createStore, compose, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage  from "redux-persist/lib/storage";
 //import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
@@ -14,21 +16,30 @@ withA(2,3) // 3+2-4 = 1
 */
 
 const loggerMiddleware = (store) => (next) => (action) => {
-    if(!action.type){
-        return next()
-    }
+  if (!action.type) {
+    return next();
+  }
 
-    console.log('type: ', action.type)
-    console.log('payload: ', action.payload)
-    console.log('currentState: ', store.getState())
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
 
-    next(action)
+  next(action);
 
-    console.log('NewState: ', store.getState())
-} 
+  console.log("NewState: ", store.getState());
+};
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // root-reducer
 const middleWares = [loggerMiddleware];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const persistor = persistStore(store);
