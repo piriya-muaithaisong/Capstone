da49edd64ec3c4c7493cd873bafc589647acc963
Piriya <piriya.muaithaisong@gmail.com>
2022-12-10 23:57:11 +0700

    migrate category to thunk

--- capstone-react-w-redux/src/routes/shop/shop.component.jsx
+++ capstone-react-w-redux/src/routes/shop/shop.component.jsx
@@ -4,22 +4,15 @@ import Category from "../category/category.component";
 import "./shop.styles.scss";
 
 import { useEffect } from "react";
-import { getCategoryAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/categories.action";
+
+import { fetchCategoriesAsync } from "../../store/categories/categories.action";
 import { useDispatch } from "react-redux";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoriesArray = await getCategoryAndDocuments();
-      //console.log(categoriesArray);
-      dispatch(setCategories(categoriesArray));
-      //console.log(categoryMap);
-    };
-
-    getCategoriesMap();
+    dispatch(fetchCategoriesAsync());
   }, []);
 
   return (
