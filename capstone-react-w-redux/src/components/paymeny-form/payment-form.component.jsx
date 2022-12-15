42d6b69f1d1f0a184e41e0e56df09aec93b87cdd
Piriya <piriya.muaithaisong@gmail.com>
2022-12-15 21:38:35 +0700

    Complete serverless stripe function and create payment process

--- capstone-react-w-redux/src/components/paymeny-form/payment-form.component.jsx
+++ capstone-react-w-redux/src/components/paymeny-form/payment-form.component.jsx
@@ -15,12 +15,38 @@ const PaymentForm = () => {
       return;
     }
 
-    
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+
+    const clientSecret = response.paymentIntent.client_secret;
+    console.log(clientSecret);
+
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Piriya",
+        },
+      },
+    });
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment successful");
+      }
+    }
   };
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay Now</Button>
