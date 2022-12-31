9459f243981ca2651562ffa69f978bb1607179a8
Piriya <piriya.muaithaisong@gmail.com>
2022-12-31 18:07:36 +0700

    Add dynamic import

--- capstone-redux-Typescript/src/App.js
+++ capstone-redux-Typescript/src/App.js
@@ -1,14 +1,23 @@
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
+//import Home from "./routes/home/home.component";
+// import Navigation from "./routes/navigation/navigation.component";
 import { Route, Routes } from "react-router-dom";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import CheckOut from "./routes/checkout/checkout.component";
+// import Authentication from "./routes/authentication/authentication.component";
+// import Shop from "./routes/shop/shop.component";
+// import CheckOut from "./routes/checkout/checkout.component";
 import { useDispatch } from "react-redux";
 
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
+import Spinner from "./components/spinner/spinner.component";
 
 import { checkUserSession } from "./store/user/user.action";
+
+const Home = lazy(() => import("./routes/home/home.component"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication.component")
+);
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const CheckOut = lazy(() => import("./routes/checkout/checkout.component"));
+const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -18,14 +27,16 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<CheckOut />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<CheckOut />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
